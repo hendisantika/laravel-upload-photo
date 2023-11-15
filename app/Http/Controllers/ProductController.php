2c15c1d5b3d01643f355c3ae4a612badf2d8ea2b
hendisantika <hendisantika@yahoo.co.id>
2023-11-15 07:32:08 +0700

    #11 - Add edit in Product Controller 🫸🌀✏️📗🐧🐳

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -62,9 +62,9 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Product $product): View
     {
-        //
+        return view('products.edit', compact('product'));
     }
 
     /**
