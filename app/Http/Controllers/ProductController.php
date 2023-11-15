8740ad0f3ee29a23b1435a9605f83a9579bea648
hendisantika <hendisantika@yahoo.co.id>
2023-11-15 07:30:24 +0700

    #7 - Add index in Product Controller 🫸🌀✏️📗🐧🐳

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -11,7 +12,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::latest()->paginate(5);
+        return view('products.index', compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
