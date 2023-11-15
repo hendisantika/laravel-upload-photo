8f32b6f146e42f1725185e5ec99ea965273d2019
hendisantika <hendisantika@yahoo.co.id>
2023-11-15 07:28:16 +0700

    #3 - Modify Products Table 🫸🌀✏️📗🐧🐳

--- database/migrations/2023_11_15_002731_create_products_table.php
+++ database/migrations/2023_11_15_002731_create_products_table.php
@@ -12,6 +12,9 @@ return new class extends Migration {
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('detail');
+            $table->string('image');
             $table->timestamps();
         });
     }
